3ccdab22bcdab9bac9364140b60b197fca31d74f
Shivam Singh <shivamdotsingh@gmail.com>
2023-10-15 02:06:45 +0530

    first commit

---
+++ src/pages/CartPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Cart from '../features/Cart/Cart'
+const CartPage = () => {
+  return (
+    <div>
+      <cart></cart>
+    </div>
+  )
+}
+
+export default CartPage
